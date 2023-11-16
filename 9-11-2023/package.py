3985489604ecb6dcde7d18205426d5e9e1d43f22
neethupdas <146073373+neethupdas@users.noreply.github.com>
2023-11-16 16:13:57 +0530

    Add files via upload

---
+++ 9-11-2023/package.py
@@ -0,0 +1,10 @@
+from mypackage import summodule as sum1
+from mypackage import average as avg1
+from mypackage import power as pow1
+
+a=sum1.sum(10,20)
+b=avg1.average(10,20,30)
+c=pow1.power(1,2)
+print(a)
+print(b)
+print(c)
